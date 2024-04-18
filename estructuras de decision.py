2aa818da84059426340294d4be7e8658419c16ab
ignaciojgr <167448938+ignaciojgr@users.noreply.github.com>
2024-04-18 19:21:36 +0000

    Se hacen cambios a la opción 2

--- estructuras de decision.py
+++ estructuras de decision.py
@@ -1,5 +1,5 @@
-print("bienvenido a estructuras de decision \n elija etapa a ejecuccion! \n 1. sistema edad \n 2. usuario/contraseña \n 3. notas \n 4. animales \n 5. formulario ")
-
+print("Bienvenido a estructuras de decision \n Elija etapa a ejecutarse! \n\n 1. Sistema edad \n 2. Usuario/Contraseña \n 3. Notas \n 4. Animales \n 5. Formulario ")
+print("\n\n")
 try: 
     choice=int(input("Ingrese el número de opción:\n"))
 except ValueError:
@@ -12,25 +12,29 @@ op3=3
 op4=4
 op5=5
 
-if choice== 1:
-    print("ejecutandose 'sistema de edad'")
+if choice==1:
+    print("ejecutandose 'sistema de edad'\n")
     edadUsuario = int(input("ingresa tu edad!:\n"))
     if edadUsuario >= 18: 
         print("usted cuenta con la mayoria de edad c: ")
     else:
         print("usted no cuenta con la mayoria de edad :c")
-if choice== 2: 
-    print("ejecutandose 'usuario/contraseña'")
-    user1 = "Pedro"
-    user2 = "Angel"
-    contraseñauser1 = 1234
-    contraseñauser2 = "a4s1"
+if choice==2: 
+    print("ejecutándose 'usuario/contraseña'")
+    user1="Pedro"
+    user2="Angel"
+    contraseñauser1=1234
+    contraseñauser2="a4s1"
 
-    usuarioNuevo=(input("ingresa tu usuario \n"))2
-    if usuarioNuevo == user1  or  usuarioNuevo == user2 :
-        print("ingrese contraseña")
+    usuarioNuevo=input("ingresa tu usuario\n")
+    contraseñaNueva=input("ingresa tu contraseña\n")
+    if usuarioNuevo==user1  and contraseñaNueva==contraseñauser1 or usuarioNuevo==user2 and contraseñaNueva=="a4s1":
+        print("sus credenciales son válidas")
     else : 
-        print("este usuario no existe")
+        while usuarioNuevo!=user1  and contraseñaNueva!=contraseñauser1 or usuarioNuevo!=user2 and contraseñaNueva!="a4s1":
+            print("Por favor, vuelva a ingresar sus credenciales")
+            usuarioNuevo=input("ingresa tu usuario \n")
+            contraseñaNueva=input("ingresa tu contraseña \n")
  
  
 
