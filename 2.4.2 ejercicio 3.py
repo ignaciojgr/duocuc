574dc8918babab44597a252db8e3652b94c3647e
ignaciojgr <167448938+ignaciojgr@users.noreply.github.com>
2024-04-24 14:33:45 +0000

    ready

--- 2.4.2 ejercicio 3.py
+++ 2.4.2 ejercicio 3.py
@@ -1,1 +1,16 @@
 #Manejo de errores de valor y de división por cero
+try:
+    numerador=float(input("Ingrese el numerador de la fracción:\n"))
+except ValueError:
+    numerador=float(input("Por favor, sólo ingrese números, no texto, por ejemplo: 5 o 5.0\n"))
+    denominador=float(input("Ingrese el denominador de la fracción:\n"))
+try:
+    denominador=float(input("Ingrese el denominador de la fracción:\n"))
+except ValueError:
+    denominador=float(input("Por favor, sólo ingrese números, no texto, por ejemplo: 5 o 5.0\n"))
+    
+resultado=0
+try:
+    resultado=numerador/denominador
+except ZeroDivisionError:
+    print(resultado)
