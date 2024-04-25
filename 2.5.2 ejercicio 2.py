78b7969bcdeb25c422eb4a54abbbfb3be809b012
ignaciojgr <167448938+ignaciojgr@users.noreply.github.com>
2024-04-25 15:59:58 +0000

    yes

--- 2.5.2 ejercicio 2.py
+++ 2.5.2 ejercicio 2.py
@@ -6,12 +6,17 @@ while sw==True:
         print("======")
         print("Seleccione una opción:\n1. Ver mi saldo\n2. Retirar dinero\n3. Salir")
         op=int(input("Por favor, elija una opción:\t"))
-        if op==1:
-            print("Tiene un saldo de $500.000")
-            sw=int(input("Presione 1 si desea continuar o 2 si desea salir:\t"))
-        elif op==2:
-            print("Transferencia realizada")
-            sw=int(input("Presione 1 si desea continuar o 2 si desea salir:\t"))
+        if op>0 and op<4:
+            if op==1:
+                print("Tiene un saldo de $500.000")
+                sw=int(input("Presione 1 si desea continuar o 2 si desea salir:\t"))
+            elif op==2:
+                print("Transferencia realizada")
+                sw=int(input("Presione 1 si desea continuar o 2 si desea salir:\t"))
+            else:
+                sw=2
+        else:
+            print("Ingreso fuera de rango")
     except ValueError:
         print("Ingreso Erróneo")
 print("Ciere de sesión exitoso, adiós")
