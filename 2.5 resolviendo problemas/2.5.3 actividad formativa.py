9ee1d3e40560164536ac6097536e5684fdf98937
ignaciojgr <167448938+ignaciojgr@users.noreply.github.com>
2024-04-25 19:22:04 +0000

    ok

--- 2.5 resolviendo problemas/2.5.3 actividad formativa.py
+++ 2.5 resolviendo problemas/2.5.3 actividad formativa.py
@@ -1,3 +1,24 @@
-kill=0
-while kill=0:
-    print("El")
+end=1
+deuda=100000
+saldo=0
+nuevo_saldo=0
+while end==1:
+    print("Bienvenido al menú del Banco de DuocUC")
+    print("\n=============\nSeleccione una opción:\n1. Pagar monto\n2. Simular una compra")
+    op=int(input("Seleccione una opción:\t"))
+    if op==1:
+        print("Usted tiene una deuda de: $",deuda)
+        pago=int(input("Monto a pagar:\t$"))
+        saldo=deuda-pago
+        print("Nuevo saldo de la tarjeta:\t",saldo)
+    if op==2:
+        cantidad_simulada=int(input("Por favor, ingrese la cantidad de compras que quiere simular:\t"))
+        for i in range(cantidad_simulada):
+            numero_de_compra=i+1
+            print("Compra número 1:\n================")
+            monto_de_compra=int(input("Ingrese el monto de la compra:\t"))
+            saldo_compra=saldo-monto_de_compra
+            nuevo_saldo=nuevo_saldo-saldo_compra
+            saldo=nuevo_saldo
+            print("Su nuevo saldo es:\t",nuevo_saldo)
+            
