ae8078c85a1413212a6e6a85f89a2f7fbf6cc943
ignaciojgr <167448938+ignaciojgr@users.noreply.github.com>
2024-04-29 15:02:53 +0000

    ok

--- 2.5 resolviendo problemas/2.5.5 actividad formativa.py
+++ 2.5 resolviendo problemas/2.5.5 actividad formativa.py
@@ -9,3 +9,31 @@ while end==True:
     contraseña1=0
     contraseña2=0
     contraseña3=0
+    opcion=int(input("Ingrese la opción que desea realizar: "))
+    if opcion==1:
+        print("Iniciar sesión")
+        usuario=input("Ingrese su usuario: ")
+        contraseña=input("Ingrese su contraseña: ")
+        if usuario==usuario1 and contraseña==contraseña1:
+            print("Bienvenido",usuario)
+        elif usuario==usuario2 and contraseña==contraseña2:
+            print("Bienvenido",usuario)
+        elif usuario==usuario3 and contraseña==contraseña3:
+            print("Bienvenido",usuario)
+        else:
+            print("Usuario o contraseña incorrectos")
+    elif opcion==2:
+        print("Registro de usuario")
+        usuario=input("Ingrese el usuario que desea registrar: ")
+        contraseña=input("Ingrese la contraseña que desea registrar: ")
+        print("Usuario registrado exitosamente.")
+        print("Usuario:",usuario)
+        print("Contraseña:",contraseña)
+    elif opcion==3:
+        print("Saliendo del sistema...")
+        end=2
+    else:
+        print("Opción no válida.\nUsted será redirigido al menú de inicio de sesión.")
+        
+    
+print("Cierre de sesión exitoso.")
