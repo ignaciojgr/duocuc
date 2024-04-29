a89e0b5b73eb2b17154c43a906b81dcd071cd865
ignaciojgr <167448938+ignaciojgr@users.noreply.github.com>
2024-04-29 14:27:00 +0000

    ok

---
+++ 2.5 resolviendo problemas/2.5.5 actividad formativa.py
@@ -0,0 +1,11 @@
+end=1
+
+while end==True:
+    print("Bienvenido al menú de inicio de sesión")
+    print(("1. Iniciar sesión\n2. Registrar usuario\n3. Salir"))
+    usuario1=0
+    usuario2=0
+    usuario3=0
+    contraseña1=0
+    contraseña2=0
+    contraseña3=0
