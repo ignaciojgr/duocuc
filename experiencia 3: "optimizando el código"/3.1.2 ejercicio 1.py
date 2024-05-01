ba113a5c0557ef96d45f23a5904de6229ebb8918
ignaciojgr <167448938+ignaciojgr@users.noreply.github.com>
2024-05-01 14:53:02 +0000

    yes

---
+++ experiencia 3: "optimizando el código"/3.1.2 ejercicio 1.py
@@ -0,0 +1,6 @@
+nombre1=input("Ingrese su nombre:\t")
+nombre2=input("Ingrese su nombre:\t")
+nombre3=input("Ingrese su nombre:\t")
+lista_nombres=[nombre1,nombre2,nombre3]
+print("Los nombres ingresados son:",lista_nombres)
+print("Fin del programa")
