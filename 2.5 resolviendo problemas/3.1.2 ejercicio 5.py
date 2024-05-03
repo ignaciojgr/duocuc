24abdbde17f80297b26fc6211b4d139da7170876
ignaciojgr <167448938+ignaciojgr@users.noreply.github.com>
2024-05-03 16:11:18 +0000

    ok

---
+++ 2.5 resolviendo problemas/3.1.2 ejercicio 5.py
@@ -0,0 +1,15 @@
+def menu():
+    print("Bienvenido al menú del almacen")
+    while True:
+        op=int(input("Ingrese una opción:\n1. Agregar producto\n2. Ver canasta2. Ver canasta\n3. Ver total\n4. Salir\n"))
+        if op==1:
+            print("Arroz: $1500"
+                  "Agua: $3000"
+                  "Carne: $7500"
+                  "Pollo: $5000"
+                  "Pan: 1000"
+                )
+            op=int(input("Ingrese la opción del producto:\n"))
+                    
+                    
+                     
