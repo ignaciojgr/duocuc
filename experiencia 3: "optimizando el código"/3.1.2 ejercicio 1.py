2e74d55a4fcfa72c1b397f751ed8dc6855bfa9a8
ignaciojgr <167448938+ignaciojgr@users.noreply.github.com>
2024-05-03 13:06:13 +0000

    ok

--- experiencia 3: "optimizando el código"/3.1.2 ejercicio 1.py
+++ experiencia 3: "optimizando el código"/3.1.2 ejercicio 1.py
@@ -1,4 +1,4 @@
-lista_nombres=[1,2,3]
-for i in range(0,len(lista_nombres)):
-    list.append(lista_nombres[i])
-    nombres=input("Ingrese un nombre: ")
+lista_nombres=[]
+for i in range(3):
+    nombre=input("Ingrese un nombre: ")
+    list.append(nombre)
