67a3d0a2dafb36d44da4ccd35919c1f4617bcfb1
ignaciojgr <167448938+ignaciojgr@users.noreply.github.com>
2024-05-04 16:03:17 +0000

    yes

--- 2.5 resolviendo problemas/3.1.2 ejercicio 5.py
+++ 2.5 resolviendo problemas/3.1.2 ejercicio 5.py
@@ -35,11 +35,20 @@ def menu():
             else:
                 print("Opción inválida. Por favor, intente nuevamente.")
         elif op == 2:
+            #La función items() de los diccionarios devuelve una lista de tuplas con las claves y los valores.
+            #Esto permite recorrer el diccionario y obtener tanto la clave como el valor.
+            #En este caso, se recorre el diccionario y se imprime la clave y el valor.
+            #El f"" es para poder interpolar variables dentro de un string.
+            #Si no se utilizara la función items(), se tendría que hacer un bucle for para recorrer las claves y luego obtener el valor.
+            #Lo anterior se puede hacer con la función values() de los diccionarios, pero no se obtendría la clave.
+            #Por lo tanto, se tendría que hacer un bucle for para recorrer las claves y luego obtener el valor.
             for producto, precio in canasta.items():
                 print(f"{producto}: ${precio}")
         elif op == 3:
+            #Esta función permite sumar todos los valores del diccionario.
             total = sum(canasta.values())
             print(f"Total: ${total}")
+            #Podría ser sin el f...
         elif op == 4:
             break
         else:
