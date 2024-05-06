d0fc3bad4f4d2dae37635728844c388d6a67eae9
ignaciojgr <167448938+ignaciojgr@users.noreply.github.com>
2024-05-06 21:09:57 +0000

    ok

--- 2.5 resolviendo problemas/3.1.5 ejercicio 2.py
+++ 2.5 resolviendo problemas/3.1.5 ejercicio 2.py
@@ -1,17 +1,26 @@
+import random
+sw=3
 numeros_participantes=[]
-for i in range(50):
-    numeros_participantes.append(i+1)
-    if i==49:
-        break
 eleccion_del_jugador=[]
+for i in range(50):
+        numeros_participantes.append(i+1)
+        if i==49:
+            break
 for i in range(7):
-    eleccion_del_jugador.append(int(input("Ingrese 7 números: ")))
-    if i==6:
-        break
-print("Los números que usted ingresó son: ",eleccion_del_jugador)
-print("Los números sorteados esta ronda son: ",numeros_participantes)
-for j in range(len(numeros_participantes)):
-    print(numeros_participantes[j])
+        eleccion_del_jugador.append(int(input("Ingrese 7 números: ")))
+        if i==6:
+            break
+for j in range(3):
+    print("Los números que usted ingresó son: ",eleccion_del_jugador)
+    numeros_ganadores=random.sample(numeros_participantes, 7)
+    print("Los números sorteados esta ronda son: ",numeros_ganadores)
+    if eleccion_del_jugador==numeros_ganadores:
+        print("¡Felicidades! Usted ha ganado el premio mayor.")
+        sw=0
+    else:
+        print("Lo sentimos, no ha ganado esta ronda.")
+    
+
 
 
 
