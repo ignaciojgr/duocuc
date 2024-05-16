cf33223ea307548049c6c5c3e33449c448ddb608
ignaciojgr <167448938+ignaciojgr@users.noreply.github.com>
2024-05-16 23:10:11 +0000

    ok

---
+++ ejercicios python/3.3/3.3.2 ejercicio 2.py
@@ -0,0 +1,5 @@
+#Construir programa que permita almacenar los run ganadores del sorteo “la suerte de un
+#run”. Los run ganadores son todos los run que terminan en los últimos dígitos antes del
+#digito verificado sean 92, 95 y 84
+#Los datos deben ser leídos desde el archivo csv llamado “listadoRun.csv” y depositados
+#en el archivo llamado “ganadores.json”
