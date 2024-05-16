9bca534d2f198d3daee5a049c41b5f933d329776
ignaciojgr <167448938+ignaciojgr@users.noreply.github.com>
2024-05-16 23:12:22 +0000

    ok

--- ejercicios python/3.3/3.3.2 ejercicio 2.py
+++ ejercicios python/3.3/3.3.2 ejercicio 2.py
@@ -3,3 +3,15 @@
 #digito verificado sean 92, 95 y 84
 #Los datos deben ser leídos desde el archivo csv llamado “listadoRun.csv” y depositados
 #en el archivo llamado “ganadores.json”
+listadoRun=[]
+ganadores=[]
+with open('listadoRun.csv') as archivo_csv:
+    lector_csv = csv.reader(archivo_csv)
+    next(lector_csv)
+    for linea in lector_csv:
+        run = linea[0]
+        if run[-2:] == '92' or run[-2:] == '95' or run[-2:] == '84':
+            ganadores.append(run)
+        listadoRun.append(run)
+with open('ganadores.json', 'w') as archivo_json:
+    json.dump(ganadores, archivo_json, indent=4)
